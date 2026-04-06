37d1073e11527886c9d850fc2e8bf4bc4fd739e4
Mazharul <mazharul.jatri@gmail.com>
2026-04-06 15:20:10 +0600

    mod

--- src/modules/mail/mail.module.ts
+++ src/modules/mail/mail.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { MailerModule } from '@nestjs-modules/mailer';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/adapters/handlebars.adapter';
 import { join } from 'path';
 import { MailService } from './services/mail/mail.service';
 
